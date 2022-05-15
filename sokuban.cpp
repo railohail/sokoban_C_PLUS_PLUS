98f478e2bbbb3f126d49207df1f1925b02df7b3d
rail_hail <102628186+railohail@users.noreply.github.com>
2022-05-15 20:15:45 +0800

    Update sokuban.cpp
    almost done!! 5/15

--- sokuban.cpp
+++ sokuban.cpp
@@ -7,6 +7,8 @@
 
 int main()
 {
+    std::vector<std::string> control_map[100]; 
+    int player_x,player_y;
     std::fstream map;// this will be the new map for watching the whole thing 
     map.open("map.txt");
     if(map.fail()) std::cout << "the map has failed to open\n";
@@ -27,9 +29,47 @@ int main()
     //problem 1. need to figure out how to only get one item from the txt file
     sokuban_test.generatemap(&target,&temp_target,height,width);
     sokuban_test.print(&target,height,width);// the initial map that the  user gets 
+    // now it's time to let the target start to move 
+    int input = 0;
+    std::string output;
+    int flag = 0;
+    sokuban_test.controlmapgenerate(control_map,&player_x,&player_y,&target,height,width);
+    // we now have the player x and player y and vector map;
+    int win;
+    while(1)
+    {
+        input = getch();
+        if(input != 0)// get rid of the NULL input
+        {    
+            // std::cout << "the input is " << input << "\n";
+            output = sokuban_test.sokuban_read_input(input,&flag);
+            if(flag == 1) break;//when you type ESC button you could leave right away     
+            // std::cout << "the output is " << output << "\n";
+            // std::cout << "the flag is " << flag << "\n";
+            //you now got the output of your arrow sign now change the map
+            
+            // std::cout << "you are at x = " << player_x << " y = "<< player_y <<"\n";
+            
+            sokuban_test.moveornot(control_map,&player_x,&player_y,height,width,output);
+            
+            
 
-
-
-
+            //this moves with every switch
+            //now write a something to show the vector
+            sokuban_test.showcurrent(control_map,height,width);
+            win = sokuban_test.did_you_win(control_map,height,width);
+            if(win == 1)
+            {
+                system("cls");
+                std::cout << "You win!!!\n";
+                return 0;
+            }
+            
+        
+        
+        
+        }
+    }
+    Sleep(5000);
     sokuban_test.end(&target);
 }
